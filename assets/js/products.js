2af1ab08597d6ce9584af913effebf73e86bc449
caike21 <caike.dev@gmail.com>
2022-03-26 18:55:19 -0300

    initial commit

---
+++ assets/js/products.js
@@ -0,0 +1,122 @@
+const jsonNhak = `{"products": [
+            {
+                "type": "potato",
+                "name": "Bolonhesa com catupiry",
+                "desc": "batata, purê Nhak, bolonhesa, catupiry, queijo e batata palha",
+                "price":"R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Bolonhesa com cheddar",
+                "desc": "batata, purê Nhak, bolonhesa, cheddar, queijo e batata palha",
+                "price":"R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Carne Louca com catupiry",
+                "desc": "batata, purê Nhak, carne cozida e desfiada com molho de tomate e azeitona, catupiry, queijo e batata palha",
+                "price":"R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Carne Louca com cheddar",
+                "desc": "batata, purê Nhak, carne cozida e desfiada com molho de tomate e azeitona, catupiry, queijo e batata palha",
+                "price":"R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Calabresa com catupiry",
+                "desc": "batata, purê Nhak, calabresa, catupiry, queijo e batata palha",
+                "price":"R$22,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Calabresa com cheddar",
+                "desc": "batata, purê Nhak, calabresa, cheddar, queijo e batata palha",
+                "price":"R$22,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Frango com catupiry",
+                "desc": "batata, purê Nhak, frango desfiado, catupiry, queijo e batata palha",
+                "price":"R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Frango com cheddar",
+                "desc": "batata, purê Nhak, frango desfiado, cheddar, queijo e batata palha",
+                "price":"R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Mexicana com catupiry",
+                "desc": "batata, purê Nhak, carne moída com molho picante, catupiry, queijo, doritos e batata palha",
+                "price":"R$24,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Mexicana com cheddar",
+                "desc": "batata, purê Nhak, carne moída com molho picante, cheddar, queijo, doritos e batata palha",
+                "price":"R$24,99"
+            },
+
+            {
+                "type": "potato",
+                "name": " Pizza com catupiry",
+                "desc": "batata, purê Nhak, presunto, queijo, tomate, orégano, catupiry e batata palha",
+                "price":" R$23,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Quatro queijos",
+                "desc": "batata, purê Nhak, mussarela, catupiry, provolone, cheddar e batata palha",
+                "price":"R$22,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Strogonoff  de carne",
+                "desc": "batata, purê Nhak, strogonoff  de carne, queijo e batata palha",
+                "price":"R$24,99"
+            },
+
+            {
+                "type": "potato",
+                "name": "Strogonoff  de frango",
+                "desc": "batata, purê Nhak, strogonoff  de frango, queijo e batata palha",
+                "price":"R$24,99"
+            },
+
+            {
+                "type": "drink",
+                "name": "Coca-Cola",
+                "desc": "Lata 350ml",
+                "price":"R$4,00"
+            },
+
+            {
+                "type": "drink",
+                "name": "Guaraná",
+                "desc": "Lata 350ml",
+                "price":"R$4,00"
+            },
+
+            {
+                "type": "drink",
+                "name": "Suco",
+                "desc": "Uva Lata 350ml",
+                "price":"R$5,00"
+            }
+                
+        ]}`;
+
